caa38d300fceb68fcde73547627d2ae2c6ce51a4
Yohanes Harke Wauran <harkewauran23@gmail.com>
2022-06-09 15:36:08 +0800

    get all destinations

--- controller/index.js
+++ controller/index.js
@@ -12,5 +12,16 @@ module.exports = {
     
             res.status(201).json({ success: true, message: 'Berhasil insert data!' });
         });
-    }
+    },
+    getAllDestinations : (req, res) => {
+        const querySql = 'SELECT * FROM destinasi_wisata';
+
+        db.query(querySql, (err, rows, field) => {
+            if (err) {
+                return res.status(500).json({ message: 'Ada kesalahan', error: err });
+            }
+    
+            res.status(200).json({ success: true, data: rows });
+        });
+    },
 }
--- router/index.js
+++ router/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router()
 
 const { 
     addDestination,
+    getAllDestinations
 } = require('../controller/index.js');
 
 router.post('/destination', addDestination);
+router.get('/destinations', getAllDestinations);
 
 module.exports = router;
