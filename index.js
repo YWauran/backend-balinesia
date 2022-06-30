413cf08dbdff470513080578d3d2955508538ced
Yohanes Harke Wauran <harkewauran23@gmail.com>
2022-06-30 17:19:43 +0800

    add  app.set

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
-const PORT = 5000;
+const PORT = process.env.PORT ||  '8080';
 
 const Router = require('./router/index');
 // set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
 app.use(cors());
+app.set("port", PORT);
 
 app.use('/api', Router);
 
 app.get('/', (req, res) => res.send('<h1>Hello World</h1>'));
 
 // buat server nya
-app.listen(process.env.PORT || PORT, () => console.log(`Server running at port: ${PORT}`));
+app.listen(PORT, () => console.log(`Server running at port: ${PORT}`));
